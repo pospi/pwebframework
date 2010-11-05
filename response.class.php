4b86127378c477105002bece30b09b5ccca040a3
Sam Pospischil <pospi@spadgos.com>
2010-11-05 11:48:46 +1000

    Implemented first round of Response class methods

--- response.class.php
+++ response.class.php
@@ -2,18 +2,114 @@
  /*===============================================================================
 	pWebFramework - response handler class
 	----------------------------------------------------------------------------
-	An abstract response class for handling nonspecific output functionality.
-	This class should be subclassed to provide specific output formatting for
-	each type of request mode (HTML, CLI, AJAX etc).
+	An abstracted response class for handling nonspecific output functionality.
+	
+	Allows setting and manipulating HTTP headers & page output blocks. Nothing
+	is affected until the response is actually sent, leaving headers clean for
+	the duration of script.
+	
+	To create more complex output logic, output blocks should be implemented as
+	string-convertible objects, with specific data formatting for request mode
+	types handled internally.
 	----------------------------------------------------------------------------
 	@author		Sam Pospischil <pospi@spadgos.com>
 	@date		2010-07-08
   ===============================================================================*/
 
-abstract class Response
+class Response
 {
-	//==================================================================================================================
+	// response types (feel free to add others)
+	const RESP_HTML		= 0;
+	const RESP_JSON		= 1;
+	const RESP_PLAINTEXT= 2;
+	
+	private	$headers = array();
+	private $outputBlocks = array();
+	
+	//==========================================================================
+	//		Output handling
+	
+	/**
+	 * Add some generic output block to the response.
+	 *
+	 * @param	mixed	$block	the output block to add. This may be:
+	 * 							- any string to echo as-is
+	 * 							- an object to be decoded via its __toString() method
+	 * 							- a nested array of the above to be decoded together
+	 * @param	int		$at		position within existing output blocks to add.
+	 * 							if the index exists, block is inserted there and all following are shifted up
+	 */
+	public function addBlock($block, $at = -1)
+	{
+		array_splice($this->outputBlocks, $at, 0, $block);
+	}
+	
+	// Send this entire response, then exit the script
+	public function send()
+	{
+		$this->sendHeaders();
+		echo $this->getOutput();
+		exit;
+	}
+	
+	/**
+	 * Compute and retrieve our output by decoding output blocks.
+	 *
+	 * @see Response::addBlock()
+	 */
+	public function getOutput($blocks = null)
+	{
+		$output = "";
+		
+		if ($blocks === null) {
+			$blocks = $this->outputBlocks;
+		}
+		
+		foreach ($blocks as $block) {
+			if (is_object($block) && method_exists($block, '__toString')) {
+				$output .= $block->__toString();
+			} else if (is_array($block)) {
+				$output .= $this->getOutput($block);
+			} else {
+				$output .= $block;
+			}
+			$output .= "\n";
+		}
+		
+		return $output;
+	}
+	
+	//==========================================================================
 	//		HTTP header handling
+	
+	public function redirect($uri)
+	{
+		$this->setHeader('Location', $uri);
+	}
+	
+	// set an HTTP header directly
+	public function setHeader($key, $val)
+	{
+		$this->headers[$key] = $val;
+	}
+	
+	public function getHeaders()
+	{
+		return $this->headers;
+	}
+	
+	public function getHeader($key)
+	{
+		return isset($this->headers[$key]) ? $this->headers[$key] : null;
+	}
+	
+	public function sendHeaders()
+	{
+		Response::checkHeaders();
+		foreach ($this->headers as $name => $value) {
+			header("$name: $value");
+		}
+	}
 
 	// Call this to abort a script if headers have already been sent
 	public static function checkHeaders()
