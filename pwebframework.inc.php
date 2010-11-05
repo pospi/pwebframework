b96e771e8cb41742646856778b5406ef09d4e4f6
Sam Pospischil <pospi@spadgos.com>
2010-11-05 11:50:53 +1000

    Added include script to grab everything

---
+++ pwebframework.inc.php
@@ -0,0 +1,20 @@
+<?php
+ /*===============================================================================
+	pWebFramework
+	----------------------------------------------------------------------------
+	A lightweight, low-level web framework for rapid & robust application development.
+	
+	Provides:
+		- Request interrogation
+		- Script input sanitisation
+		- Session management
+		- Response handling
+	----------------------------------------------------------------------------
+	@author		Sam Pospischil <pospi@spadgos.com>
+	@date		2010-10-28
+  ===============================================================================*/
+ 
+	require_once('request.class.php');
+	require_once('response.class.php');
+	require_once('session.class.php');
+?>
