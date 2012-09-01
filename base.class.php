6062f9cbe296677904749b4e45bb2fbd7493241a
pospi <pospi@spadgos.com>
2012-09-01 17:07:26 +1000

    added a simple classloader function to the base framework class

--- base.class.php
+++ base.class.php
@@ -2,7 +2,14 @@
 /**
  * pWebFramework shared class
  *
- * Only contains global configuration vars
+ * Contains global configuration vars and the super-simple classloader system, which
+ * just does nothing if classes are already loaded, or require()s the relevant file
+ * from this directory otherwise.
+ * Due to the structure of the framework, cross-dependencies for other framework classes
+ * will be automatically require()d upon loading the class file of interest.
+ *
+ * @package pWebFramework
+ * @author Sam Pospischil
  */
 class pwebframework
 {
@@ -17,6 +24,25 @@ class pwebframework
 	public static $dbaseExceptionClass = null;
 
 	public static $PWF_PATH;
+
+	/**
+	 * Class loader for framework classes, to avoid re-loading the same
+	 * classes once already included elsewhere in the currently executing script.
+	 * @param  string $className name of the class to load, as it appears in PHP
+	 * @return TRUE if the class was already loaded, or FALSE if it needed to be.
+	 *              If the class is not found, a fatal error will automatically be thrown by require().
+	 */
+	public function loadClass($className)
+	{
+		if (class_exists($className)) {
+			return true;
+		}
+
+		$filename = strtolower($className . '.class.php');
+		require_once(pwebframework::$PWF_PATH . $filename);
+
+		return false;
+	}
 }
 
 pwebframework::$PWF_PATH = dirname(__FILE__) . '/';
